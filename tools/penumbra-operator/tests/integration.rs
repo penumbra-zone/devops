5766c4384dd53ee4ebbd77fb3fb8e6e3c686ddcd
Conor Schaefer <conor@penumbralabs.xyz>
2025-02-15 11:18:33 -0800

    test: use minikube for integration testing
    Had to wrestle with the context pattern quite a bit, but finally settled
    on `test-context` which is much more pleasantly than manually cobbling
    together arc/sync/once_cell and custom drop impls.

--- tools/penumbra-operator/tests/integration.rs
+++ tools/penumbra-operator/tests/integration.rs
@@ -10,20 +10,18 @@
 // # - [ ] services have endpoints
 // # - [ ] dns resolves in a multi-validator setup
 // # - [ ]
-//
+
 use penumbra_operator::configure_tracing;
 use std::process::Command;
+mod common;
+use common::cleanup_resource;
+use common::TEST_NAMESPACE;
 
-/// The k8s namespace in which all test resources will be created.
-/// Must be unique to the test suite, because all resources in this
-/// namespace will be destroyed!
-//
-// TODO: figure out how to make this unique
-const TEST_NAMESPACE: &str = "penumbra-operator-testing";
-// const TEST_NAMESPACE: &str = "penumbra";
+use crate::common::TestProcesses;
+use test_context::test_context;
 
 /// Remove all test resources. This is a VERY DESTRUCTIVE action.
-async fn cleanup() -> anyhow::Result<()> {
+async fn cleanup_networks() -> anyhow::Result<()> {
     configure_tracing()?;
 
     // Delete all PenumbraNetworks.
@@ -40,51 +38,17 @@ async fn cleanup() -> anyhow::Result<()> {
     Ok(())
 }
 
-/// Reusable function to handle deleting all instances of a resource.
-/// Optionally can take additional args, to support filtering, e.g.
-/// by label.
-fn cleanup_resource(resource_type: &str, args: Vec<String>) -> anyhow::Result<()> {
-    // Get list of all requested resource types.
-    let output = Command::new("kubectl")
-        .args(["-n", TEST_NAMESPACE, "get", resource_type, "-o", "name"])
-        .args(args)
-        .output()?;
-    assert!(
-        output.status.success(),
-        "failed to get existing {}s: {}",
-        resource_type,
-        String::from_utf8_lossy(&output.stderr)
-    );
-    let s = String::from_utf8_lossy(&output.stdout);
-    let resources: Vec<&str> = s
-        .trim_end_matches('\n')
-        .split('\n')
-        .filter(|x| !x.is_empty())
-        .collect();
-    tracing::debug!("found {} {}", resources.len(), resource_type);
-
-    // Remove any PenumbraNetworks..
-    if resources.is_empty() {
-        return Ok(());
-    }
-    let status = Command::new("kubectl")
-        .args(["-n", TEST_NAMESPACE, "delete", "--wait"])
-        .args(&resources)
-        .status()?;
-    assert!(status.success(), "failed to delete {}", resource_type);
-
-    Ok(())
-}
-
+#[test_context(TestProcesses)]
 #[tokio::test]
-async fn cleanup_passes() -> anyhow::Result<()> {
-    cleanup().await?;
+async fn cleanup_networks_passes(_ctx: &TestProcesses) -> anyhow::Result<()> {
+    cleanup_networks().await?;
     Ok(())
 }
 
+#[test_context(TestProcesses)]
 #[tokio::test]
 /// Apply an example CRD for network.
-async fn create_network_via_crd() -> anyhow::Result<()> {
+async fn create_network_via_crd(_ctx: &TestProcesses) -> anyhow::Result<()> {
     let status = Command::new("kubectl")
         .args([
             "-n",
