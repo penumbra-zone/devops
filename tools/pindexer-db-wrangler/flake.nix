8dc05a0236010616a22a725c3dc93b40923a6395
Conor Schaefer <conor@penumbralabs.xyz>
2025-02-25 18:49:39 -0800

    chore: bump mainnet pindexer version to 1.1.0

--- tools/pindexer-db-wrangler/flake.nix
+++ tools/pindexer-db-wrangler/flake.nix
@@ -10,8 +10,7 @@
   # We declare two different versions of upstream `pindexer`
   # deps, to enable handling different network environments differently.
   inputs.penumbra-repo-mainnet = {
-    # url = "github:penumbra-zone/penumbra/v1.0.2";
-    url = "github:penumbra-zone/penumbra/main";
+    url = "github:penumbra-zone/penumbra/v1.1.0";
     # Reuse the nixpkgs from the current devshell.
     inputs.nixpkgs.follows = "nixpkgs"; # Use your nixpkgs
   };
