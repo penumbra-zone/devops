39b51182e80473c245f0e4c9962d404db7105afb
Conor Schaefer <conor@penumbralabs.xyz>
2025-06-12 14:57:49 -0700

    chore: bump penumbra deps

--- tools/pindexer-db-wrangler/flake.nix
+++ tools/pindexer-db-wrangler/flake.nix
@@ -10,13 +10,13 @@
   # We declare two different versions of upstream `pindexer`
   # deps, to enable handling different network environments differently.
   inputs.penumbra-repo-mainnet = {
-    url = "github:penumbra-zone/penumbra/v1.5.2";
+    url = "github:penumbra-zone/penumbra/v1.5.3";
     # Reuse the nixpkgs from the current devshell.
     inputs.nixpkgs.follows = "nixpkgs"; # Use your nixpkgs
   };
 
   inputs.penumbra-repo-testnet = {
-    url = "github:penumbra-zone/penumbra/v2.0.0-alpha.11";
+    url = "github:penumbra-zone/penumbra/v2.0.0-alpha.12";
     # Reuse the nixpkgs from the current devshell.
     inputs.nixpkgs.follows = "nixpkgs"; # Use your nixpkgs
   };
